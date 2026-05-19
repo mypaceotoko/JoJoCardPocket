f1de26f032f8474009a99965a55a3fbaa45fa792
mypaceotoko <mypaceotoko@gmail.com>
2026-05-20 07:37:48 +0900

    Create Stand Pocket static web app with JoJo-themed UI

---
+++ fallback-data.js
@@ -0,0 +1,12 @@
+window.FALLBACK_DATA = {
+  characters: [
+    { id: '1', name: 'Jotaro Kujo', japaneseName: '空条承太郎', chapter: 'Part 3', abilities: '超人的な判断力', nationality: 'Japanese', catchphrase: 'やれやれだぜ', living: true, isHuman: true },
+    { id: '2', name: 'Dio Brando', japaneseName: 'ディオ・ブランドー', chapter: 'Part 1-3', abilities: '吸血鬼の力', nationality: 'British', catchphrase: '無駄無駄無駄！', living: false, isHuman: false },
+    { id: '3', name: 'Josuke Higashikata', japaneseName: '東方仗助', chapter: 'Part 4', abilities: '修復能力', nationality: 'Japanese', catchphrase: 'ドララララ！', living: true, isHuman: true }
+  ],
+  stands: [
+    { id: '1', name: 'Star Platinum', japaneseName: 'スタープラチナ', chapter: 'Part 3+', abilities: '超スピードと精密動作', standUser: 'Jotaro Kujo', battlecry: 'オラオラオラ！' },
+    { id: '2', name: 'The World', japaneseName: 'ザ・ワールド', chapter: 'Part 3', abilities: '時を止める', standUser: 'DIO', battlecry: 'WRYYYY!' },
+    { id: '3', name: 'Crazy Diamond', japaneseName: 'クレイジー・ダイヤモンド', chapter: 'Part 4', abilities: '治癒・修復', standUser: 'Josuke Higashikata', battlecry: 'ドラララ！' }
+  ]
+};
